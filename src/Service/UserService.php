d8052129ecc8a91d16ab0636798b73559e3aa96f
Amit Kumar Das <its.amit.das@gmail.com>
2022-05-30 15:41:55 +0530

    [TASK] create dynamic password and send mail to user

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -8,18 +8,21 @@ use App\Entity\Candidate;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 class UserService
 {
-    public function __construct(Private EntityManagerInterface $entityManager, Private UserRepository $userRepository)
+    public function __construct(Private EntityManagerInterface $entityManager, Private UserRepository $userRepository, private MailerInterface $mailer)
     {
     }
 
     public function createUser(Candidate $candidate): User
     {
         $user = new User();
+        $plainPassword = $this->createPassword();
         $user->setRoles(['ROLE_EMPLOYEE'])
-            ->setPassword($this->createPassword())
+            ->setPassword($plainPassword)
             ->setEmail($candidate->getEmail())
             ->setName($candidate->getName())
             ->setMobile($candidate->getMobile())
@@ -27,13 +30,33 @@ class UserService
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        if($user){
+            $this->sendMail($user,$plainPassword);
+        }
+
         return $user;
     }
 
-    private function createPassword(){
+    private function createPassword(): string
+    {
         $carecterSet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $password = substr(str_shuffle($carecterSet), 0, 6);
         return $password;
     }
 
+    public function sendMail($user,$plainPassword){
+
+        $email = (new TemplatedEmail())
+            ->from("its.amit.das@gmail.com")
+            ->to($user->getEmail())
+            ->subject("Test for email send")
+            ->htmlTemplate("email/userCredentialMailTemplate.html.twig")
+            ->context([
+                "user" => $user,
+                "plainPassword" => $plainPassword
+            ]);
+
+        $this->mailer->send($email);
+    }
 }
